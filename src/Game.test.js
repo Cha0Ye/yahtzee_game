3437105ab1d6f91a70bc074fd413b79ca1fb5dc5
Chao Ye <Chao12.ye@gmail.com>
2019-03-22 12:06:19 -0700

    add test to catch lock/unlock bug

--- src/Game.test.js
+++ src/Game.test.js
@@ -6,3 +6,18 @@ import Game from './Game';
 it('renders without crashing', () => {
   mount(<Game />);
 });
+
+
+// test to catch lock/unlock bug
+
+
+
+it('should lock die so it does not re-roll', () => {
+  let wrapper = mount(<Game />);
+  //console.log('wrapper is ',wrapper.debug())
+  
+  wrapper.setState({dice:[1,2,3,4,5]})
+
+  wrapper.find('Die').first().simulate('click', {target: {value: 1}})
+  
+});
